f38b1ff986ad377f0306727ac01f3e699b775e76
Eraser3 <helemaalgeennut3@gmail.com>
2023-06-03 14:28:11 +0200

    Update plugin.py
    Changed Current Power Usage device so that it also accumulates total energy usage.
    Fixed problem with communication interval.
    Changed settings so there is only one settings for communication interval.

--- plugin.py
+++ plugin.py
@@ -1,44 +1,18 @@
 ##           HomeWizard Wi-Fi P1 Meter Plugin
 ##
 ##           Author:         Eraser
-##           Version:        0.0.3
-##           Last modified:  13-01-2023
+##           Version:        1.0.1
+##           Last modified:  31-05-2023
 ##
 """
-<plugin key="HomeWizardWifiP1Meter" name="HomeWizard Wi-Fi P1 Meter" author="Eraser" version="0.0.3" externallink="https://www.homewizard.nl/p1-meter">
+<plugin key="HomeWizardWifiP1Meter" name="HomeWizard Wi-Fi P1 Meter" author="Eraser" version="1.0.1" externallink="https://www.homewizard.nl/p1-meter">
     <description>
-        <h3>Devices</h3>
-        <ul style="list-style-type:square">
-            <li><b>Current usage</b>: Displays the current power usage in watts. Can de negative when you are producing more power then you are using. 5 minute values are logged.</li>
-            <li><b>Total electricity</b>: Shows usage and return for both tariffs.</li>
-            <li><b>Current usage</b>: Shows usage and return for both tariffs.</li>
-            <li><b>Usage per phase</b>: Shows usage and return for all phases.</li>
-            <li><b>Voltage per phase</b>: Shows the current voltage for all phases.</li>
-            <li><b>Amperage per phase</b>: Shows the current amperage for all phases.</li>
-            <li><b>Total gas</b>: Displays your gas usage per day and total.</li>
-            <li><b>Production switch</b>: Automatically turns On when you are delivering power to the grid. This means that you are producing more power than you are currently using.</li>
-            <li><b>Production value switch</b>: Automatically turns On when you are delivering the minimum specified amount of power to the grid. Set the minimum value in the settings below.</li>
-            <li><b>Usage value switch</b>: Automatically turns On when you are drawing the minimum specified amount of power from the grid. Set the minimum value in the settings below.</li>
-            <li><b>Wifi signal strength</b>: Displays the wifi signal strength for the p1 meter.</li>
-        </ul>
-        <h3>Configuration</h3>
+        
     </description>
     <params>
         <param field="Address" label="IP Address" width="200px" required="true" default="127.0.0.1" />
         <param field="Port" label="Port" width="200px" required="true" default="80" />
         <param field="Mode1" label="Data interval" width="200px">
-            <options>
-                <option label="10 seconds" value="10"/>
-                <option label="20 seconds" value="20"/>
-                <option label="30 seconds" value="30"/>
-                <option label="1 minute" value="60" default="true"/>
-                <option label="2 minutes" value="120"/>
-                <option label="3 minutes" value="180"/>
-                <option label="4 minutes" value="240"/>
-                <option label="5 minutes" value="300"/>
-            </options>
-        </param>
-        <param field="Mode2" label="Switch interval" width="200px">
             <options>
                 <option label="10 seconds" value="10"/>
                 <option label="20 seconds" value="20"/>
@@ -71,16 +45,11 @@ class BasePlugin:
     #Plugin variables
     pluginInterval = 10     #in seconds
     dataInterval = 60       #in seconds
-    switchInterval = 60     #in seconds
     dataIntervalCount = 0
-    switchIntervalCount = 0
     usageSwitchValue = 0
     productionSwitchValue = 0
     
     #Homewizard P1 meter variables
-    #smr_version = -1                #: [Number] De SMR versie van de meter
-    meter_model = ""                #: [String] Het merk en type indicatie van de slimme meter
-    wifi_ssid = ""                  #: [String] Het Wi-Fi netwerk waarmee de P1 meter is verbonden
     wifi_strength = -1              #: [Number] De sterkte van het Wi-Fi signaal in %
     total_power_import_t1_kwh = -1  #: [Number] De stroom afname meterstand voor tarief 1 in kWh
     total_power_import_t2_kwh = -1  #: [Number] De stroom afname meterstand voor tarief 2 in kWh
@@ -100,6 +69,7 @@ class BasePlugin:
     gas_timestamp = -1              #: [Number] De datum en tijd van de meest recente gas meterstand gestructureerd als YYMMDDhhmmss.
     
     #Calculated variables
+    total_power = 0                 #: Het totale gecombineerde vermogen.
     import_active_power_w = 0       #: Het huidig vermogen wat momenteel van het net wordt geimporteerd.
     export_active_power_w = 0       #: Het huidig vermogen wat momenteel naar het net wordt geexporteerd.
     
@@ -133,13 +103,6 @@ class BasePlugin:
             # If not, set to 60 sec.
             self.dataInterval = 60
             
-        # If switch interval between 10 sec. and 5 min.
-        if 10 <= int(Parameters["Mode2"]) <= 300:
-            self.switchInterval = int(Parameters["Mode2"])
-        else:
-            # If not, set to 60 sec.
-            self.switchInterval = 60
-            
         # If usage switch value
         if isNumber(Parameters["Mode3"]) == True and 1 <= int(Parameters["Mode3"]) <= 999999:
             self.usageSwitchValue = int(Parameters["Mode3"])
@@ -163,65 +126,53 @@ class BasePlugin:
         return True
 
     def onMessage(self, Data, Status, Extra):
-        self.dataIntervalCount += self.pluginInterval
-        self.switchIntervalCount += self.pluginInterval
-        
-        #------- Collect data -------
-        if ( self.dataIntervalCount >= self.dataInterval or self.switchIntervalCount >= self.switchInterval ):
-            try:
-                Domoticz.Debug("Reading electricity values from input")
-                
-                #self.smr_version = Data['smr_version']
-                self.meter_model = Data['meter_model']
-                self.wifi_ssid = Data['wifi_ssid']
-                self.wifi_strength = Data['wifi_strength']
-                self.total_power_import_t1_kwh = int(Data['total_power_import_t1_kwh'] * 1000)
-                self.total_power_import_t2_kwh = int(Data['total_power_import_t2_kwh'] * 1000)
-                self.total_power_export_t1_kwh = int(Data['total_power_export_t1_kwh'] * 1000)
-                self.total_power_export_t2_kwh = int(Data['total_power_export_t2_kwh'] * 1000)
-                self.active_power_w = Data['active_power_w']
-                
-                if ( 'active_power_l1_w' in Data ): self.active_power_l1_w = Data['active_power_l1_w']
-                if ( 'active_power_l2_w' in Data ): self.active_power_l2_w = Data['active_power_l2_w']
-                if ( 'active_power_l3_w' in Data ): self.active_power_l3_w = Data['active_power_l3_w']
-                if ( 'active_voltage_l1_v' in Data ): self.active_voltage_l1_v = float(Data['active_voltage_l1_v'])
-                if ( 'active_voltage_l2_v' in Data ): self.active_voltage_l2_v = float(Data['active_voltage_l2_v'])
-                if ( 'active_voltage_l3_v' in Data ): self.active_voltage_l3_v = float(Data['active_voltage_l3_v'])
-                if ( 'active_current_l1_a' in Data ): self.active_current_l1_a = float(Data['active_current_l1_a'])
-                if ( 'active_current_l2_a' in Data ): self.active_current_l2_a = float(Data['active_current_l2_a'])
-                if ( 'active_current_l3_a' in Data ): self.active_current_l3_a = float(Data['active_current_l3_a'])
-                
-                Domoticz.Debug("Reading gas values from input")
-                
-                #New version has room for multiple gas meters. We only monitor the first one if available.
-                for external in Data['external']:
-                    if ( external['type'] == "gas_meter" ):
-                        self.total_gas_m3 = int(external['value'] * 1000)
-                        self.gas_timestamp = external['timestamp']
-                        break
-                    
-                Domoticz.Debug("Calculating active power")
-                
-                if ( self.active_power_w >= 0 ):
-                    self.import_active_power_w = self.active_power_w
-                    self.export_active_power_w = 0
-                else:
-                    self.import_active_power_w = 0
-                    self.export_active_power_w = self.active_power_w * -1
-            except:
-                Domoticz.Error("Failed to read response data")
-                return
-        
-        #------- Graphs -------
-        if ( self.dataIntervalCount >= self.dataInterval ):
-            self.dataIntervalCount = 0
+        try:
+            Domoticz.Debug("Reading electricity values from input")
             
+            self.wifi_strength = Data['wifi_strength']
+            self.total_power_import_t1_kwh = int(Data['total_power_import_t1_kwh'] * 1000)
+            self.total_power_import_t2_kwh = int(Data['total_power_import_t2_kwh'] * 1000)
+            self.total_power_export_t1_kwh = int(Data['total_power_export_t1_kwh'] * 1000)
+            self.total_power_export_t2_kwh = int(Data['total_power_export_t2_kwh'] * 1000)
+            self.active_power_w = Data['active_power_w']
+            
+            if ( 'active_power_l1_w' in Data ): self.active_power_l1_w = Data['active_power_l1_w']
+            if ( 'active_power_l2_w' in Data ): self.active_power_l2_w = Data['active_power_l2_w']
+            if ( 'active_power_l3_w' in Data ): self.active_power_l3_w = Data['active_power_l3_w']
+            if ( 'active_voltage_l1_v' in Data ): self.active_voltage_l1_v = float(Data['active_voltage_l1_v'])
+            if ( 'active_voltage_l2_v' in Data ): self.active_voltage_l2_v = float(Data['active_voltage_l2_v'])
+            if ( 'active_voltage_l3_v' in Data ): self.active_voltage_l3_v = float(Data['active_voltage_l3_v'])
+            if ( 'active_current_l1_a' in Data ): self.active_current_l1_a = float(Data['active_current_l1_a'])
+            if ( 'active_current_l2_a' in Data ): self.active_current_l2_a = float(Data['active_current_l2_a'])
+            if ( 'active_current_l3_a' in Data ): self.active_current_l3_a = float(Data['active_current_l3_a'])
+            
+            Domoticz.Debug("Reading gas values from input")
+            
+            #New version has room for multiple gas meters. We only monitor the first one if available.
+            for external in Data['external']:
+                if ( external['type'] == "gas_meter" ):
+                    self.total_gas_m3 = int(external['value'] * 1000)
+                    self.gas_timestamp = external['timestamp']
+                    break
+                
+            Domoticz.Debug("Calculating active power")
+            
+            self.total_power = self.total_power_import_t1_kwh + self.total_power_import_t2_kwh - self.total_power_export_t1_kwh - self.total_power_export_t2_kwh
+            
+            if ( self.active_power_w >= 0 ):
+                self.import_active_power_w = self.active_power_w
+                self.export_active_power_w = 0
+            else:
+                self.import_active_power_w = 0
+                self.export_active_power_w = self.active_power_w * -1
+                
             #------- Power -------
             try:
                 if ( self.active_power_id not in Devices ):
                     Domoticz.Device(Name="Current power usage",  Unit=self.active_power_id, Type=243, Subtype=29).Create()
                     
-                UpdateDevice(self.active_power_id, 0, numStr(self.active_power_w) + ";0", True)
+                #UpdateDevice(self.active_power_id, 0, numStr(self.active_power_w) + ";0", True)
+                UpdateDevice(self.active_power_id, 0, numStr(self.active_power_w) + ";" + numStr(self.total_power), True)
             except:
                 Domoticz.Error("Failed to update device id " + str(self.active_power_id))
             
@@ -334,9 +285,7 @@ class BasePlugin:
             except:
                 Domoticz.Error("Failed to update device id " + str(self.wifi_signal_id))
             
-        #------- Switches -------
-        if ( self.switchIntervalCount >= self.switchInterval ):
-            self.switchIntervalCount = 0
+            #------- Switches -------
             
             try:
                 if ( self.switch_export_id not in Devices ):
@@ -372,7 +321,10 @@ class BasePlugin:
                         UpdateDevice(self.switch_import_value_id, 0, "Off")
                 except:
                     Domoticz.Error("Failed to update device id " + str(self.switch_import_value_id))
-               
+        except:
+            Domoticz.Error("Failed to read response data")
+            return
+           
         return True
                     
     def onCommand(self, Unit, Command, Level, Hue):
@@ -384,7 +336,13 @@ class BasePlugin:
         return
 
     def onHeartbeat(self):
-        self.readMeter()
+        self.dataIntervalCount += self.pluginInterval
+        
+        #------- Collect data -------
+        if ( self.dataIntervalCount >= self.dataInterval ):
+            self.dataIntervalCount = 0
+            self.readMeter()
+        
         return
 
     def onDisconnect(self):
